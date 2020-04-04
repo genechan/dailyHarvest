0fd1617665c3869233ea1b22e1732cd2b5219df3
Gene Chan <genechanemail@yahoo.com>
2020-04-03 20:02:37 -0400

    added Redux

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,12 @@
-const reducer = (state = { test: "foobar" }, { type, payload }) => {
-  return { ...state };
+import { USER_INPUT } from "./actions";
+
+const reducer = (state = {}, { type, payload }) => {
+  switch (type) {
+    case USER_INPUT:
+      return { userInput: payload };
+    default:
+      return { ...state };
+  }
 };
 
 export default reducer;
